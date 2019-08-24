b6d38fa046ae67f1eb55d6c2a87e258865cfc4e6
FeierSarah <2109625761@qq.com>
2019-08-24 16:44:59 +0800

    Signed-off-by: FeierSarah <2109625761@qq.com>

--- 8.19 test 二分匹配/最小支配级.cpp
+++ 8.19 test 二分匹配/最小支配级.cpp
@@ -5,12 +5,10 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-const int maxn = 1000 + 5;
+const int maxn=1010;
 int pre[maxn];
 bool visit[maxn];
 int newpos[maxn];
-int now;
-int n, m;
 int head[maxn];
 struct Edge
 {
@@ -24,6 +22,39 @@ void addedge(int u,int v)
 	edge[cnt].next=head[u]; 
 	head[u]=cnt++;
 }
+int now;
+void dfs(int x)
+{
+	newpos[now++]=x;
+	for(int k=head[x];k!=-1;k=edge[k].next)
+	{
+		if(!visit[edge[k].to])
+		{
+			visit[edge[k].to]=true;
+			pre[edge[k].to]=x;
+			dfs(edge[k].to);
+		}
+	}
+}
+int n;
+int mvc()
+{
+	bool s[maxn]={0};
+	bool set[maxn]={0};
+	int ans=0;
+	for(int i=n-1;i>=1;i--)
+	{
+		int t=newpos[i];
+		if(!s[t]&&!s[pre[t]])
+		{
+			set[pre[t]]=true;
+			ans++;
+			s[t]=true;
+			s[pre[t]]=true;
+		}
+	}
+	return ans;
+}
 void init()
 {
 	memset(head,-1,sizeof head);
@@ -33,57 +64,35 @@ void init()
 	visit[0]=true;
 	pre[0]=0;
 }
-void DFS(int x) {
-    newpos[now ++] = x;
-    for(int k = head[x]; k != -1; k = edge[k].next) {
-        if(!visit[ edge[k].to ]) {
-            visit[ edge[k].to ] = true;
-            pre[edge[k].to] = x;
-            DFS(edge[k].to);
-        }
-    }
-}
-int MVC() 
-{
-    bool s[maxn] = {0};
-    bool set[maxn] = {0};
-    int ans = 0;
-    for(int i = n - 1; i >= 1; i--) {
-        int t = newpos[i];
-        if(!s[t] && !s[ pre[t] ]) {
-            set[ pre[t] ] = true;
-            ans ++; 
-            s[t] = true;
-            s[ pre[t] ] = true;
-        }        
-    }
-    return ans;
-}
 void solve() 
 {
     memset(visit, false, sizeof(visit));
     now = 0;
     visit[1] = true;
     pre[1] = 1;
-    DFS(1);
-    MVC();
+    dfs(1);
+    mvc();
 }
 int main()
 {
 	int T; 
 	while(cin>>T&&T>0);
 	{
-		int u,v,M;
+		init();
 		while(T--)
 		{
+			int u,v,M;
 			cin>>u>>M;
 			while(M>0)
 			{
+				int v;
 				cin>>v;
+				addedge(u,v);
 				M--;
 			}
 		}
-		cout<<MVC()<<endl;
+		dfs(0);
+		cout<<mvc()<<endl;
 	}
 	return 0;
  } 
