f7e8602b515245584e665f61b025253a3f9a72a2
FeierSarah <2109625761@qq.com>
2020-10-03 13:47:19 +0800

    Signed-off-by: FeierSarah <2109625761@qq.com>

---
+++ test jw 服务器端/homework05/src/homework/CustomerDAO.java
@@ -0,0 +1,36 @@
+package homework;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class CustomerDAO {
+	private Connection con;
+	public CustomerDAO() 
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url="jdbc:mysql://localhost:3306/test";
+			 con=DriverManager.getConnection(url,"test","1");
+		}
+		catch(Exception e) 
+		{
+			
+		}
+	}
+	public boolean insertCustomer(CustomerBean customer) throws SQLException 
+	{
+		Statement st=con.createStatement();
+		String sql="insert into customer values('"+customer.getCustName()+"','"+customer.getEmail()+"','"
+				+customer.getPhone()+"')";
+		boolean rs=st.execute(sql);
+		if(!rs) 
+		{
+			return true;
+		}
+		return false;
+	}
+
+}
+
